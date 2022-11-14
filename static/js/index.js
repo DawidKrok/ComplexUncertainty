5211a5b9fb90463d72ca513c779d967de454c347
Dawid Krok <privatedawidk@gmail.com>
2022-11-14 10:38:44 +0100

    Display evaluated LaTeX

--- static/js/index.js
+++ static/js/index.js
@@ -1,38 +1,61 @@
 class Param {
-    constructor(n, v, u) {
+    constructor(n, v, u, c=false) {
         this.name = n
         this.value = v
         this.uncer = u
+        this.is_const = c
     }
 }
 
+/// TODO: check if formula contains parameters that aren't in scope
+/// TODO: Use is_const flag to skip derivatives of constant terms
 
 // ===== | GENERATE FORMULA | =====
 formula_in.onchange = () => {
     params = get_params() 
+
+    scope = new Map()
+    for(p of params) { scope.set(p.name, p.value) }
     
     // ===== | GENERATE LATEX | =====
-    latex = "u(y) = \\sqrt{"
-    for(p of params) {
+    general_latex = "u(y) = \\sqrt{"
+    evaluated_latex = "\\sqrt{"
+    whole_expr = 0  // to determine final value
 
-        derivative = math.derivative(formula_in.value, p.name).toTex()
+    // iterate through all non constant params
+    for(p of params.filter(pa => !pa.is_const)) {
 
-        // skip and remove all non-contributing params
-        if(derivative == "0")  {
+        derivative = math.derivative(formula_in.value, p.name)
+
+        // skip and remove all non-contributing params (the ones listed in params list, but not included in formula)
+        if(derivative.toTex() == "0")  {
             params = params.filter(par => par.name != p.name)
             continue
         }
 
         // don't put '+' sign before the first part
-        if(p.name != params[0].name)   latex += " + "
-
-        latex += `\\Big(\\big(  ${derivative}  \\big)\\:  u(${p.name})  \\Big)^2`
+        if(p.name != params[0].name) {
+            general_latex += " + "
+            evaluated_latex += " + "
+        }  
         
+        expr = derivative.compile()
+
+        general_latex += `\\Big(\\big(  ${derivative.toTex()}  \\big)\\:  u(${p.name})  \\Big)^2`
+        evaluated_latex += `\\big( ${expr.evaluate(scope).toFixed(4) } \\cdot ${p.uncer} \\big)^2`
+
+        whole_expr += Math.pow(expr.evaluate(scope) * p.uncer, 2)
+        console.log(whole_expr, expr.evaluate(scope), p.uncer)
     }
-    latex += "}"
+    general_latex += "}"
+    evaluated_latex += "}"
     
+
+    // ========= | DISPLAY LATEX | ========
+    latex =`${general_latex} = ${evaluated_latex} = ${ Math.sqrt(whole_expr).toFixed(4) }`
+
     tex_btn.value = latex
-    tex.innerHTML = `\\[ ${latex} \\]`
+    tex.innerHTML = `\\[ ${ latex } \\]`
 
     MathJax.typesetPromise()
 }
@@ -40,7 +63,10 @@ formula_in.onchange = () => {
 // Copy the LaTex inside of button 
 tex_btn.onclick = () => { navigator.clipboard.writeText(tex_btn.value) }
 
-// ===== | PARAMS | =====
+
+
+// ================== | PARAMS | =================
+
 
 // Add Param Card
 param_add.onclick = () => {
@@ -52,6 +78,7 @@ param_add.onclick = () => {
     params_container.append(node[0])
 }
 
+// gets parameters' values from all Param Cards
 get_params = () => {
     params = []
     
@@ -60,6 +87,7 @@ get_params = () => {
         v = $( elem ).find(".param_value").val()
         u = $( elem ).find(".param_uncer").val()
 
+        // add param only if all values are populated
         if(n != "" && v != "" && u != "") {  
             params.push(new Param(n, v, u))
         }
